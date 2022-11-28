1bd98eb8476bb90f85ad44d75d04ac43fe68c9c2
cookiedream <105692097+cookiedream@users.noreply.github.com>
2022-11-28 16:32:00 +0800

    表單連接完成

--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/courses";
+const API_URL = "http://localhost:8888/api/courses";
 
 class CourseService {
   post(title, description, price) {
